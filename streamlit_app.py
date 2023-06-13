91e6bdafcb0f320b3e3237f931432b08d923dcbf
swathi7k <93930628+swathi7k@users.noreply.github.com>
2023-06-13 14:31:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,7 @@ streamlit.title('Sherlocks Daily routine')
 streamlit.header('Morning routine')
 streamlit.text('Wake up at 6:00 AM')
 streamlit.text('Going to gym')
+streamlit.text('Make avocado,banana smoothie')
+import pandas
+my_fruit_list=pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.dataframe(my_fruit_list)
