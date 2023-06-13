898467bdf49bab96fbe14df6849798aab8eacd6a
swathi7k <93930628+swathi7k@users.noreply.github.com>
2023-06-13 15:17:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,5 @@ streamlit.text('Make avocado,banana smoothie')
 import pandas
 my_fruit_list=pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 streamlit.dataframe(my_fruit_list)
-streamlit.multiselect("Pick your fruits:",list(my_fruit_list(index)))
+streamlit.multiselect("Pick your fruits:",list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
