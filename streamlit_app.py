aab56b5d844a9765ded4de4d9cf9a388cc179bd1
swathi7k <93930628+swathi7k@users.noreply.github.com>
2023-06-13 13:31:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit
 streamlit.title('Sherlocks Daily routine')
 
 streamlit.header('Morning routine')
-streamlit.text('Wake up 6:00 AM')
-Streamlit.text('Going to gym')
+streamlit.text('Wake up at 6:00 AM')
+streamlit.text('Going to gym')
